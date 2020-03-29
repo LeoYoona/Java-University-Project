c71aa0515999ed160d79e4e049782c3195f264b8
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-03-29 21:05:21 +0200

    stuff2

--- src/JavaUniversityProject/HelloFX.java
+++ src/JavaUniversityProject/HelloFX.java
@@ -1,17 +1,33 @@
 package JavaUniversityProject;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+/*
+import JavaUniversityProject.factory.MenuBuilder;
+import JavaUniversityProject.javafx.layout.GeneratedMenuMainWindow;
+import JavaUniversityProject.javafx.layout.MenuMainWindow;
+import JavaUniversityProject.model.Role;*/
+
+
+
 public class HelloFX extends Application {
 	public static void main(String[] args) {
 		launch();
 	}
-}
+
 
 	@Override
-	public void start(Stage window) throws Exception {
+	/*public void start(Stage window) throws Exception {
 		window.setTitle("Hello JavaFX");
 		Button button = new Button("Click me");
 		button.setPrefWidth(300);
@@ -19,5 +35,57 @@ public class HelloFX extends Application {
 		Scene scene = new Scene(button);
 		window.setScene(scene);
 		window.show();
-		}
+		}*/
+		public void start(Stage window) throws Exception {
+		
+		window.setTitle("Login screen");
+	
+		GridPane gridPane = new GridPane();
+		gridPane.setPadding(new Insets(10, 10, 10, 10));
+		gridPane.setVgap(10);
+		gridPane.setHgap(8);
+		
+		Label userLabel = new Label();
+		userLabel.setText("Username:");
+		GridPane.setConstraints(userLabel, 0, 0);
+		
+		TextField userInput = new TextField();
+		userInput.setPromptText("username");
+		GridPane.setConstraints(userInput, 1, 0);
+		
+		Label passwordLabel = new Label("Password");
+		GridPane.setConstraints(passwordLabel,  0,  1);
+		
+	
+		PasswordField passwordField = new PasswordField();
+		GridPane.setConstraints(passwordField, 1, 1);
+	
+		String password = passwordField.getText();
+		Button loginButton = new Button();
+		loginButton.setText("Log in");
+		GridPane.setConstraints(loginButton, 1, 2);
+		
+		Scene scene = new Scene(gridPane, 300, 200);
+		
+		loginButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				if (userInput != null) {
+					System.out.println("Closing the window...");
+					window.close();
+					//new MenuMainWindow();
+					//new GeneratedMenuMainWindow(Role.ADMIN);
+				}
+				
+			}
+		});
+		
+		gridPane.getChildren().addAll(userLabel, userInput, passwordLabel, passwordField, loginButton);
+		
+		window.setScene(scene);
+		window.show();
+		
+	}
+
 }
